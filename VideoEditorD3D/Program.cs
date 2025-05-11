c661dbc576bd900a1a7fd3960d7a53f9e1791f9e
Willem Beltman <hwj.beltman@gmail.com>
2025-05-12 01:07:58 +0200

    a

--- VideoEditorD3D/Program.cs
+++ VideoEditorD3D/Program.cs
@@ -5,7 +5,6 @@ public static class Program
     [STAThread]
     static void Main()
     {
-        
         ApplicationConfiguration.Initialize();
         Thread.CurrentThread.Name = "Forms thread";
         System.Windows.Forms.Application.Run(new Direct3D.ApplicationForm(new Application.ApplicationContext()));
